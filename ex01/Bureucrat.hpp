5a36edf7b78de0e2fb86903c3bf9ac7181020c7e
ridahafidi <ridahafidi5@gmail.com>
2026-01-26 19:07:12 +0100

    need tests

--- ex01/Bureucrat.hpp
+++ ex01/Bureucrat.hpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 #include <exception>
-
+#include "Form.hpp"
 class Bureucrat
 {
 private:
@@ -33,8 +33,20 @@ public:
             return "Grade is too low!";
         }
     };
+    void signForm(Form& form);
 };
 std::ostream &operator<<(std::ostream &other, Bureucrat &Bureucrat);
 
+void Bureucrat::signForm(Form& form)
+{
+    try {
+        form.beSigned(*this);
+        std::cout << name << " signed " << form.getName() << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cout << name << " couldn't sign " << form.getName() 
+                  << " because " << e.what() << std::endl;
+    }
+}
 
 #endif
